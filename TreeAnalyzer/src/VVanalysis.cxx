0b2407aff3e0f7fd39d7429c4a0ec6e897b1956b
Daniela Schaefer <daniela.schafer@cern.ch>
2017-08-08 15:39:09 +0200

    added function for merged truth of V jets

--- TreeAnalyzer/src/VVanalysis.cxx
+++ TreeAnalyzer/src/VVanalysis.cxx
@@ -198,7 +198,24 @@ void VVanalysis::ExecuteEvent( const SInputData&, Double_t weight) throw( SError
   //-------------Select two fat jets-------------//
   std::vector<UZH::Jet> goodFatJets;
   std::vector<UZH::Jet> goodGenJets;
-
+  std::vector<UZH::GenParticle> GenQuarks;
+  
+  if(!m_isData){
+      for(int i=0;i< m_genParticle.N;i++)
+      {
+        bool containsV=0;
+        if(TMath::Abs(m_genParticle.pdgId->at(i)) > 6 or TMath::Abs(m_genParticle.pdgId->at(i)) <1) continue;
+        for(int ii=0;ii< m_genParticle.nMoth->at(i);ii++)
+        {
+           if(TMath::Abs(m_genParticle.mother->at(i).at(ii)) == 24 or TMath::Abs(m_genParticle.mother->at(i).at(ii)) == 23) containsV = 1;     
+        }
+        if (!containsV) continue;
+        UZH::GenParticle MyQuark( &m_genParticle, i );
+        GenQuarks.push_back(MyQuark);
+      }
+  }
+    
+    
   for ( int i = 0; i < (m_jetAK8.N); ++i ) {
    
     UZH::Jet myjet( &m_jetAK8, i );
@@ -306,7 +323,7 @@ void VVanalysis::ExecuteEvent( const SInputData&, Double_t weight) throw( SError
   }
   
   // nLeptonOverlap                  =           ;
-  // jj_mergedVTruth                 =           ;
+  jj_mergedVTruth                 =  (isMergedVJet(goodFatJets[0].tlv(),GenQuarks) || isMergedVJet(goodFatJets[1].tlv(),GenQuarks));
   //
 
 
@@ -319,6 +336,18 @@ void VVanalysis::ExecuteEvent( const SInputData&, Double_t weight) throw( SError
 
   return;
 
+}
+
+ bool VVanalysis::isMergedVJet(TLorentzVector goodFatJet, std::vector<UZH::GenParticle> VdecayProducts) {
+    int associatedQuarks=0;
+    for(unsigned int i=0;i< VdecayProducts.size();i++)
+    {
+      TLorentzVector q = VdecayProducts[i].tlv();
+      if (goodFatJet.DeltaR(q) < 0.8) associatedQuarks +=1;
+
+    }
+    if (associatedQuarks ==2) return 1;
+    else return 0;   
 }
 
 void VVanalysis::FillValidationHists( ValHistsType ht, const TString& status ) {  
