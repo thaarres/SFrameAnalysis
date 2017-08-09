c24f4d80ed9f250bb83ef9ca7f762edd4eb212ef
Daniela Schaefer <daniela.schafer@cern.ch>
2017-08-09 10:34:38 +0200

    added puppi+sd mass corrections

--- TreeAnalyzer/include/VVAnalysisTools.h
+++ TreeAnalyzer/include/VVAnalysisTools.h
@@ -2,11 +2,12 @@
 #define VVANALYSISTOOLS_H
 
 #include "../include/VVanalysis.h"
+#include <TF1.h>
 
 bool isMergedVJet(TLorentzVector goodFatJet, std::vector<UZH::GenParticle> VdecayProducts);
 
 void FindGeneratedQuarks(Ntuple::GenParticleNtupleObject m_genParticle, bool m_isData , std::vector<UZH::GenParticle> GenQuarks);
 
-
+float ApplyPuppiSoftdropMassCorrections(UZH::Jet puppiJet,std::vector<TF1*> m_puppisd_corr, bool m_isData);
 
 #endif
