bb3f09231175cd0410176632e00fa88713e874f5
Claudia Seitz <clseitz.physics@gmail.com>
2017-10-03 10:40:37 +0200

    highlight needed samples for WtagToptagSF

--- TreeAnalyzer/src/LumiWeight.cxx
+++ TreeAnalyzer/src/LumiWeight.cxx
@@ -38,23 +38,23 @@ double LumiWeight::getLumiWeight( TString sample ){
   if( sample.Contains( "QCD_HT2000toInf"                      ) ) return 25.25;
   if( sample.Contains( "QCD_Pt-15to7000" ) or sample.Contains( "QCD_Pt_15to7000" )) return  2.022100000e+09*60.5387252324;
   if( sample.Contains("BulkGrav") or sample.Contains("Qstar") or sample.Contains("Wprime") or sample.Contains("Zprime")) return 1.0;
-  if( sample.Contains( "TT_TuneCUETP8M2T4"                    ) ) return  831.76      ;
+  if( sample.Contains( "TT_TuneCUETP8M2T4"                    ) ) return  831.76      ;//SF
   if( sample.Contains( "TT_TuneEE5C_13TeV-powheg-herwigpp"    ) ) return  831.76      ;
-  if (sample.Contains("WJetsToLNu_HT-100To200"                ) ) return 1347*1.21    ;
-  if (sample.Contains("WJetsToLNu_HT-200To400"                ) ) return 360*1.21     ;
-  if (sample.Contains("WJetsToLNu_HT-400To600"                ) ) return 48.9*1.21    ;
-  if (sample.Contains("WJetsToLNu_HT-600To800"                ) ) return 12.08*1.21   ;
-  if (sample.Contains("WJetsToLNu_HT-800To1200"               ) ) return 5.26*1.21    ;
-  if (sample.Contains("WJetsToLNu_HT-1200To2500"              ) ) return 1.33*1.21    ;
-  if (sample.Contains("WJetsToLNu_HT-2500ToInf"               ) ) return 0.03089*1.21 ;
-  if (sample.Contains("WW_TuneCUETP8M1"                       ) ) return 118.7        ;
-  if (sample.Contains("WZ_TuneCUETP8M1"                       ) ) return 16.5         ;
-  if (sample.Contains("ZZ_TuneCUETP8M1"                       ) ) return 47.13        ;
-  if (sample.Contains("ST_s-channel_4f_leptonDecays"          ) ) return 47.13        ;
-  if (sample.Contains("ST_t-channel_top_4f_leptonDecays"      ) ) return 136.02*0.322 ;
-  if (sample.Contains("ST_t-channel_antitop_4f_leptonDecays"  ) ) return 80.95*0.322  ;
-  if (sample.Contains("ST_tW_antitop_5f_inclusiveDecays"      ) ) return 35.6         ;
-  if (sample.Contains("ST_tW_top_5f_inclusiveDecays_"         ) ) return 35.6         ;
+  if (sample.Contains("WJetsToLNu_HT-100To200"                ) ) return 1347*1.21    ;//SF
+  if (sample.Contains("WJetsToLNu_HT-200To400"                ) ) return 360*1.21     ;//SF
+  if (sample.Contains("WJetsToLNu_HT-400To600"                ) ) return 48.9*1.21    ;//SF
+  if (sample.Contains("WJetsToLNu_HT-600To800"                ) ) return 12.08*1.21   ;//SF
+  if (sample.Contains("WJetsToLNu_HT-800To1200"               ) ) return 5.26*1.21    ;//SF
+  if (sample.Contains("WJetsToLNu_HT-1200To2500"              ) ) return 1.33*1.21    ;//SF
+  if (sample.Contains("WJetsToLNu_HT-2500ToInf"               ) ) return 0.03089*1.21 ;//SF
+  if (sample.Contains("WW_TuneCUETP8M1"                       ) ) return 118.7        ;//SF
+  if (sample.Contains("WZ_TuneCUETP8M1"                       ) ) return 16.5         ;//SF
+  if (sample.Contains("ZZ_TuneCUETP8M1"                       ) ) return 47.13        ;//SF
+  if (sample.Contains("ST_s-channel_4f_leptonDecays"          ) ) return 47.13        ;//SF
+  if (sample.Contains("ST_t-channel_top_4f_leptonDecays"      ) ) return 136.02*0.322 ;//SF
+  if (sample.Contains("ST_t-channel_antitop_4f_leptonDecays"  ) ) return 80.95*0.322  ;//SF
+  if (sample.Contains("ST_tW_antitop_5f_inclusiveDecays"      ) ) return 35.6         ;//SF
+  if (sample.Contains("ST_tW_top_5f_inclusiveDecays_"         ) ) return 35.6         ;//SF
   
   
   std::cout << "Cross section not defined for this sample!!" << std::endl;
